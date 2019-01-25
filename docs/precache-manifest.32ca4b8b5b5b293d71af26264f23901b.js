68d31e6d1b97d000e13781b8ad897512e61fbc38
alhana-yo <beatriz.german.rengel@gmail.com>
2019-01-25 13:35:32 +0100

    Docs added

---
+++ docs/precache-manifest.32ca4b8b5b5b293d71af26264f23901b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bbc610481f1e6e4c87e",
+    "url": "./static/css/main.ee983aa4.chunk.css"
+  },
+  {
+    "revision": "6bbc610481f1e6e4c87e",
+    "url": "./static/js/main.6bbc6104.chunk.js"
+  },
+  {
+    "revision": "742ec24ec510d5e04995",
+    "url": "./static/js/1.742ec24e.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "00805463c7a4fa580e5beaeb8923cc95",
+    "url": "./static/media/coffin.00805463.svg"
+  },
+  {
+    "revision": "b5275133a0a8914bf24508a54a9d5a3b",
+    "url": "./static/media/laugh.b5275133.svg"
+  },
+  {
+    "revision": "4bca91e39375637dc2bd0b8b471899d6",
+    "url": "./static/media/lion.4bca91e3.svg"
+  },
+  {
+    "revision": "340b76909529d99c98ccece71112f062",
+    "url": "./static/media/snake.340b7690.svg"
+  },
+  {
+    "revision": "b9b30ab8c6d60b689f331b283d3c800a",
+    "url": "./static/media/crow.b9b30ab8.svg"
+  },
+  {
+    "revision": "a4e76960d9d8b0ad0eb348102db3fff6",
+    "url": "./static/media/badger.a4e76960.svg"
+  },
+  {
+    "revision": "48aeaa0d3cb3117edcffe35bad4f1ab0",
+    "url": "./index.html"
+  }
+];
