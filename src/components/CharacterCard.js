15d9f553f374a24f8f76e5bf666338ec9f13421b
alhana-yo <beatriz.german.rengel@gmail.com>
2019-01-25 08:53:37 +0100

    Add all icons for houses and status, done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+
+
 class CharacterCard extends Component {
+
     render() {
         const {character} = this.props
         return (
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import GoBack from './GoBack';
 import NoData from './NoData';
+import coffin from '../icons/coffin.svg';
+import laugh from '../icons/laugh.svg';
+import lion from '../icons/lion.svg';
+import snake from '../icons/snake.svg';
+import crow from '../icons/crow.svg';
+import badger from '../icons/badger.svg';
+
 
 class CharacterDetail extends Component {
 
@@ -10,7 +17,28 @@ class CharacterDetail extends Component {
     }
 
     isAlive(alive){
-        return alive ? 'vivo' : 'muerto';
+        return alive ?  <span>Vivo<img className="status__alive" src={laugh} alt="status vivo"/></span> : <span>Muerto<img className="status__death" src={coffin} alt="status muerto"/></span>;
+        //<span role="img" aria-label="alive">🤯</span>
+        
+    }
+    getHouse(house){
+
+        if (house.toLocaleLowerCase()==='gryffindor'){
+            return <span>{house}<img className="house__img" src={lion} alt={house}/></span>
+
+        }else if(house.toLocaleLowerCase()==='slytherin'){
+            return <span>{house}<img className="house__img" src={snake} alt={house}/></span>
+
+
+        }else if(house.toLocaleLowerCase()==='ravenclaw'){
+            return <span>{house}<img className="house__img" src={crow} alt={house}/></span>
+        }else if(house.toLocaleLowerCase()==='hufflepuff'){
+            return <span>{house}<img className="house__img" src={badger} alt={house}/></span>
+        }else{
+            return 'no tiene casa'
+            
+        }
+        
     }
 
     render() {
@@ -27,11 +55,10 @@ class CharacterDetail extends Component {
                     <div className="character__detail-wrapper">
                         <img src={selectedCharacter.image} alt={selectedCharacter.name} className="character__detail-image" />
                         <h2 className="character__detail-name">{selectedCharacter.name}</h2>
-                        <p className="character__detail-house">{selectedCharacter.house}</p>
+                        <p className="character__detail-house">{this.getHouse(selectedCharacter.house)}</p>
                         <p className="character__detail-birth">{selectedCharacter.yearOfBirth}</p>
                         <p className="character__detail-patronus">{this.hasPatronus(selectedCharacter.patronus)}</p>
                         <p className="character__detail-status">{this.isAlive((selectedCharacter.alive))}</p>
-
 
                     </div>
                     <GoBack />
